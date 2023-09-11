b5f239239157ac6f3f378a7a10e5903923e5407a
yogi713 <yogesh71352@gmail.com>
2023-09-11 11:45:39 +0530

    reduced boiler plate code
    reduced boiler plate code

--- src/main/java/com/example/demo/entity/Department.java
+++ src/main/java/com/example/demo/entity/Department.java
@@ -1,40 +1,21 @@
 package com.example.demo.entity;
 
-import java.util.Arrays;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
-import org.springframework.format.annotation.DateTimeFormat;
+import lombok.*;
+
 
 @Entity
-// Data equivalent to getters, setters, hashcode(), equals() methods and argsConstructor
-@Data
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class Department {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long departmentID;
-//    @NotBlank(message = "Kindly Provide DepartmentName to query..!")
-//    @NotEmpty
-//    @NotNull
-//    @Past
-//    @PastOrPresent
-//    @Future
-//    @FutureOrPresent
-//    @DateTimeFormat
-//    @Email
-//    @NegativeOrZero
-//    @Length(max = 12, min = 0)
-//    @Size(min = 2, max = 7)
     private String departmentName;
     private String studentName;
     private Double cgpa;
